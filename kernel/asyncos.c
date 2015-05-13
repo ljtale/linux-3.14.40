b596b3827076b1375810326e1ee047ca61dddf13
Aaron Carroll <aaronc@rice.edu>
2015-05-13 02:21:31 -0500

    save the latest changes

--- kernel/asyncos.c
+++ kernel/asyncos.c
@@ -44,7 +44,6 @@ static int thread_data2 = 0;
 static struct files_struct *files[NUM_CORE];
 static struct fs_struct *fs[NUM_CORE];
 
-char log_debug = 0;
 
 /* worker thread to take over specific syscall queue*/
 static inline int 
@@ -56,7 +55,7 @@ worker(void *data){
 	struct files_struct *files_temp;
 	struct fs_struct *fs_temp;
 	smp_rmb();
-	printk("jie: worker->kernel thread for %d-th syscall queue\n", q_index);
+	trace_printk("jie: worker->kernel thread for %d-th syscall queue\n", q_index);
 	/* the following code will work on sys_queue[q_index];*/
 	for(;;){
 		int q;
@@ -77,22 +76,22 @@ exec_syscall:
 		/* FIXME: at this point, I only test read, so I know there are three arguments
 		 * but for generic syscalls, I need a way to know about the number of arguments*/	
 #ifdef TEST_READ
-		printk("jie: worker->arg1 %d, arg2 0x%lx, arg3 %lu, pid %d\n",
+		trace_printk("jie: worker->arg1 %d, arg2 0x%lx, arg3 %lu, pid %d\n",
 		(int)e->arg1, (unsigned long)e->arg2, (size_t)e->arg3, (int)sys_getpid());
-		printk(KERN_INFO "jie: worker->files 0x%lx, fs 0x%lx\n",
+		trace_printk(KERN_INFO "jie: worker->files 0x%lx, fs 0x%lx\n",
 		(unsigned long)current->files, (unsigned long)current->fs);
 
 		files_temp = current->files;
-	//	fs_temp = current->fs;
+		fs_temp = current->fs;
 	
 		current->files = files[q_index];
 	//	current->fs = fs[q_index];
 	
 		smp_wmb();
-		printk(KERN_INFO "jie: worker->files 0x%lx, fs 0x%lx\n",
+		trace_printk(KERN_INFO "jie: worker->files 0x%lx, fs 0x%lx\n",
 		(unsigned long)kt[q_index]->files, (unsigned long)kt[q_index]->fs);
 		sys_ret = sys_read((unsigned int)e->arg1, (char __user *)e->arg2, (size_t)e->arg3);
-		printk("jie: worker-> sys_read returns %d\n", (int)sys_ret);
+		trace_printk("jie: worker-> sys_read returns %d\n", (int)sys_ret);
 		smp_wmb();
 		e->status = sys_ret >=0 ? SC_COMPLETE : SC_FREE;
 		smp_wmb();
@@ -100,7 +99,7 @@ exec_syscall:
 #else
 		/* FIXME: the following code is not working at present*/
 /*
-		printk("jie: worker->syscall num %d, syscall arg cnt %d\n", e->sys_num, e->arg_cnt);
+		trace_printk("jie: worker->syscall num %d, syscall arg cnt %d\n", e->sys_num, e->arg_cnt);
 		func = (void *)*(sys_call_table + e->sys_num);
 		if(e->arg_cnt == 0){
 			func_zero = *(sys_call_table + e->sys_num);
@@ -150,7 +149,7 @@ __get_free_entry(int q_index){
 	do{
 		for( i = 2; i < QUEUE_SIZE; i++ ){
 			if(cas(SC_FREE, SC_ALLOCATED, &sys_queue[q_index].sq[i].status)){
-				printk("jie: get_free_entry-> get entry %lu\n",i);
+				trace_printk("jie: get_free_entry-> get entry %lu\n",i);
 				smp_mb();
 				return i;
 			} 
@@ -169,7 +168,7 @@ __complete(int q_index, int pos){
 	do{
 		if(e->status == SC_COMPLETE){
 			retval = (int64_t)e->arg1;
-			printk("jie: __complete-> complete pos %d in queue %d, retval %lld\n", pos, q_index, retval);
+			trace_printk("jie: __complete-> complete pos %d in queue %d, retval %lld\n", pos, q_index, retval);
 			if(cas(SC_COMPLETE, SC_FREE, &e->status)){
 				smp_mb();
 				return retval;
@@ -198,33 +197,32 @@ SYSCALL_DEFINE0(asyncos_init){
 		sys_queue[0].sq[i].status = SC_FREE;
 		sys_queue[1].sq[i].status = SC_FREE;
 	}
-	log_debug = 1;
 	smp_wmb();
-	printk("jie: init-> asyncos_init flag %d\n", init_flag);
+	trace_printk("jie: init-> asyncos_init flag %d\n", init_flag);
 	if(init_flag == 1){
-		printk("jie: init->asyncos_init is already called\n");
+		trace_printk("jie: init->asyncos_init is already called\n");
 		return retval;
 	}
-	printk(KERN_INFO "jie: init->asyncos_init !\n");
-	printk(KERN_INFO "jie: init->system call table address: 0x%lx\n",(unsigned long)(sys_call_table));
+	trace_printk(KERN_INFO "jie: init->asyncos_init !\n");
+	trace_printk(KERN_INFO "jie: init->system call table address: 0x%lx\n",(unsigned long)(sys_call_table));
 	/* create two worker threads, once create, they cannot be terminated*/
 	BUG_ON((thread_data1 > 3 || thread_data2 > 3));
 	kt[0] = kthread_create(worker, (void *)&thread_data1, "worker %d", thread_data1);
 	if(kt[0]){
 		kthread_bind(kt[0], (thread_data1 + 1) % NUM_CORE);
-		printk("jie: init->worker thread %d (id %d) on cpu %d create successfully!\n", 
+		trace_printk("jie: init->worker thread %d (id %d) on cpu %d create successfully!\n", 
 		thread_data1, kt[0]->pid, kt[0]->on_cpu);
 
 	}
 	kt[1] = kthread_create(worker, (void *)&thread_data2, "worker %d", thread_data2);
 	if(kt[1]){
 		kthread_bind(kt[1], (thread_data2 + 1) % NUM_CORE);
-		printk("jie: init->worker thread %d (id %d) on cpu %d create successfully!\n",
+		trace_printk("jie: init->worker thread %d (id %d) on cpu %d create successfully!\n",
 		thread_data2, kt[1]->pid, kt[1]->on_cpu);
 
 	}
 	smp_mb();
-	printk("jie: init->about to launch the kernel threads...\n");	
+	trace_printk("jie: init->about to launch the kernel threads...\n");	
 	wake_up_process(kt[0]);
 	wake_up_process(kt[1]);
 	init_flag = 1;
@@ -235,7 +233,7 @@ SYSCALL_DEFINE0(asyncos_init){
 SYSCALL_DEFINE4(asyncos_issue_read, unsigned int, fd, char __user *, buf, size_t, count, int, q_index){
 	size_t retval = 0;
 	struct syscall_entry *e;
-	printk(KERN_INFO "jie: issue->asyncos_issue_read!\n");
+	trace_printk(KERN_INFO "jie: issue->asyncos_issue_read!\n");
 	/* potentially we can spawn multiple threads (or pin the threads on specific cores)
 	 * and then run the sys_read on different threads*/
 	/* poplulate the queue entry*/	
@@ -247,15 +245,15 @@ SYSCALL_DEFINE4(asyncos_issue_read, unsigned int, fd, char __user *, buf, size_t
 	e->arg2 = (unsigned long)buf;
 	e->arg3 = (unsigned long)count;
 	smp_wmb();
-	printk(KERN_INFO "jie: issue->read on fd: %d, buf: 0x%lx, reading %d bytes, returning %d, pid %d\n",
+	trace_printk(KERN_INFO "jie: issue->read on fd: %d, buf: 0x%lx, reading %d bytes, returning %d, pid %d\n",
 	(int)e->arg1, (unsigned long)e->arg2, (int)e->arg3, (int)retval, (int)sys_getpid());
 	//retval = sys_read((unsigned int)e->arg1,(char __user *)e->arg2,(size_t)e->arg3);
-	//printk(KERN_INFO"***read returns %d\n", retval);
+	//trace_printk(KERN_INFO"***read returns %d\n", retval);
 	//return retval;
 	/* copy and transfer the fs and file struct information*/
 	files[q_index] = current->files;
 	fs[q_index] = current->fs;
-	printk(KERN_INFO "jie: issue->files 0x%lx, fs 0x%lx\n",(unsigned long)current->files, (unsigned long)current->fs);
+	trace_printk(KERN_INFO "jie: issue->files 0x%lx, fs 0x%lx\n",(unsigned long)current->files, (unsigned long)current->fs);
 	if(cas(SC_ALLOCATED, SC_POSTED, &e->status)){
 		return retval;
 	}
@@ -264,7 +262,7 @@ SYSCALL_DEFINE4(asyncos_issue_read, unsigned int, fd, char __user *, buf, size_t
 }
 
 SYSCALL_DEFINE2(asyncos_complete, int, q_index, int, pos){
-	printk("jie: complete->asyncos_complete! q_index: %d, pos: %d\n",q_index, pos);
+	trace_printk("jie: complete->asyncos_complete! q_index: %d, pos: %d\n",q_index, pos);
 	return __complete(q_index, pos);
 }
 
